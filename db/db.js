928a02f87dc8edb36dc883a4ec904afcf9016648
RamiroPadro <ramirop@shifta.la>
2024-09-09 14:20:44 -0300

    database

--- db/db.js
+++ db/db.js
@@ -2,11 +2,19 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'gestor-financiero',
-  password: '38050222',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
+
+pool.connect((err, client, release) => {
+  console.log('Connecting to PostgreSQL database');
+  console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connected to PostgreSQL database');
 });
 
 module.exports = pool;
